42f4040c9796e474f1cedf38fcceeba4683ac8cf
Francisco Veracoechea <veracoecheafrancisco@gmail.com>
2026-08-16 18:31:44 -0400

    vavicon

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -29,6 +29,11 @@ export const Route = createRootRoute({
 		],
 		links: [
 			{
+				rel: "icon",
+				type: "image/svg+xml",
+				href: "/favicon.svg",
+			},
+			{
 				rel: "stylesheet",
 				href: appCss,
 			},
